1c4c9733fe3c4a172f6b10668c2b4817dc2c48b6
peterfbacklund <backlundpf@state.gov>
2024-06-24 16:31:43 -0400

    migrated bulk add response

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,7 +1,11 @@
 import * as esbuild from "esbuild";
 
 await esbuild.build({
-  entryPoints: ["./src/audit_styles.css", "./src/pages/ao_db/ao_db.js"],
+  entryPoints: [
+    "./src/audit_styles.css",
+    "./src/pages/ao_db/ao_db.js",
+    "./src/pages/bulk_add_response/bulk_add_response.js",
+  ],
   bundle: true,
   minify: false,
   outdir: "dist",
