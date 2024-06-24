bcca554ac3295fd64a42b9d9f112bfb5a90aeb68
peterfbacklund <backlundpf@state.gov>
2024-06-24 14:46:28 -0400

    update build script

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,7 +1,7 @@
 import * as esbuild from "esbuild";
 
 await esbuild.build({
-  entryPoints: ["./src/audit_styles.css", "./src/pages/ao_db.js"],
+  entryPoints: ["./src/audit_styles.css", "./src/pages/ao_db/ao_db.js"],
   bundle: true,
   minify: false,
   outdir: "dist",
