cc27c8a627a3f117099d6181a821db40266eeba8
peterfbacklund <backlundpf@state.gov>
2024-06-25 09:35:11 -0400

    migrate RO_DB

--- src/pages/ro_db/ro_db.js
+++ src/pages/ro_db/ro_db.js
@@ -1,12 +1,34 @@
-﻿var Audit = window.Audit || {};
+﻿import "../../common/utilities.js";
+
+window.Audit = window.Audit || {};
 Audit.EAReport = Audit.EAReport || {};
+
+const html = String.raw;
+
+const roDbTemplate = html`
+  <div class="audit">
+    <div style="padding-bottom: 10px">
+      <a
+        title="Refresh this page"
+        href="javascript:void(0)"
+        onclick="Audit.Common.Utilities.Refresh()"
+        ><span class="ui-icon ui-icon-refresh"></span>Refresh</a
+      >
+      <span id="lblFilteredOn" style="padding-left: 10px"></span>
+    </div>
+
+    <div id="divExplorerView"></div>
+  </div>
+`;
+
+document.getElementById("app").innerHTML = roDbTemplate;
 
 var paramShowSiteActionsToAnyone = GetUrlKeyValue("ShowSiteActions");
 if (paramShowSiteActionsToAnyone != true) {
   //hide it even for owners unless this param is passed into the URL; pass in param if you want to change the page web parts/settings
   //Hide Site Actions (fail safe even though master page does it)
-  $("#RibbonContainer-TabRowLeft").hide();
-  $(".ms-siteactionsmenu").hide();
+  // document.getElementById("#RibbonContainer-TabRowLeft").style.display = "none";
+  // document.getElementById("#s4-ribbonrow").style.display = "none";
 }
 
 if (document.readyState === "ready" || document.readyState === "complete") {
@@ -14,9 +36,7 @@ if (document.readyState === "ready" || document.readyState === "complete") {
 } else {
   document.onreadystatechange = () => {
     if (document.readyState === "complete" || document.readyState === "ready") {
-      ExecuteOrDelayUntilScriptLoaded(function () {
-        SP.SOD.executeFunc("sp.js", "SP.ClientContext", InitReport);
-      }, "sp.js");
+      InitReport();
     }
   };
 }
@@ -26,14 +46,7 @@ function InitReport() {
   Audit.EAReport.Init();
 }
 
-Audit.EAReport.Init = function () {
-  var paramShowSiteActionsToAnyone = GetUrlKeyValue("ShowSiteActions");
-  if (paramShowSiteActionsToAnyone != true) {
-    //hide it even for owners unless this param is passed into the URL; pass in param if you want to change the page web parts/settings
-    $("#RibbonContainer-TabRowLeft").hide();
-    $(".ms-siteactionsmenu").hide();
-  }
-};
+Audit.EAReport.Init = function () {};
 
 Audit.EAReport.NewReportPage = function () {
   var path =
@@ -44,11 +57,11 @@ Audit.EAReport.NewReportPage = function () {
     "/" +
     Audit.Common.Utilities.GetLibTitleResponseDocsEA();
   //$("#divExplorerView").html( '<a href="#" onClick="javascript:CoreInvoke(\'NavigateHttpFolder\', \'http://cgfsauditst.rm.state.gov/sites/itaudit/AuditResponseDocsEA/\', \'_blank\');">View In Explorer</a>');
-  $("#divExplorerView").html(
-    '<span class="ui-icon ui-icon-folder-open"></span><a title="Open documents in Explorer View" href="#" onClick="javascript:CoreInvoke(\'NavigateHttpFolder\', \'' +
-      path +
-      "', '_blank');\">View In Explorer</a>"
-  );
+  // $("#divExplorerView").html(
+  //   '<span class="ui-icon ui-icon-folder-open"></span><a title="Open documents in Explorer View" href="#" onClick="javascript:CoreInvoke(\'NavigateHttpFolder\', \'' +
+  //     path +
+  //     "', '_blank');\">View In Explorer</a>"
+  // );
 
   var filterField = GetUrlKeyValue("FilterField1");
   var filterValue = GetUrlKeyValue("FilterValue1");
@@ -59,28 +72,26 @@ Audit.EAReport.NewReportPage = function () {
     var modifiedDate = new Date(filterValue);
     modifiedDate = modifiedDate.format("M/d/yyyy");
 
-    $("#lblFilteredOn").html(
+    document.getElementById("lblFilteredOn").innerHTML =
       "Filtered Documents (<b>" +
-        filterField +
-        "</b> = <b>" +
-        modifiedDate +
-        "</b>)"
-    );
+      filterField +
+      "</b> = <b>" +
+      modifiedDate +
+      "</b>)";
   } else if (
     filterField != null &&
     filterField != "" &&
     filterValue != null &&
     filterValue != ""
   ) {
-    $("#lblFilteredOn").html(
+    document.getElementById("lblFilteredOn").innerHTML =
       "Filtered Documents (<b>" +
-        filterField +
-        "</b> = <b>" +
-        filterValue +
-        "</b>)"
-    );
+      filterField +
+      "</b> = <b>" +
+      filterValue +
+      "</b>)";
   } else {
-    $("#lblFilteredOn").html("");
+    document.getElementById("lblFilteredOn").innerHTML = "";
   }
   var publicMembers = {
     //Load: m_fnLoadData
