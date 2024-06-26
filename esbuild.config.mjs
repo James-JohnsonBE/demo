63290d9e81375044f1804cd0e39df0bfaf345a6f
Peter Backlund <backlundpf@state.gov>
2024-06-26 11:26:43 -0400

    updates for new org
    update resource links for new github org

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,4 +1,6 @@
 import * as esbuild from "esbuild";
+import * as fs from "fs";
+import * as path from "path";
 
 await esbuild.build({
   entryPoints: [
@@ -16,3 +18,21 @@ await esbuild.build({
   minify: true,
   outdir: "dist",
 });
+
+const referenceFiles = [
+  "pages/ao_db/AO_DB.txt",
+  "pages/bulk_add_response/BulkAddResponse.txt",
+  "pages/ia_db/IA_DB.txt",
+  "pages/permissions/Permissions.txt",
+  "pages/qa_db/QA_DB.txt",
+  "pages/ro_db/RO_DB.txt",
+  "pages/sp_db/SP_DB.txt",
+  "pages/update_site_groups/UpdateSiteGroups.txt",
+];
+
+referenceFiles.forEach(copyReferenceFiles);
+function copyReferenceFiles(filePath) {
+  const srcTextFile = path.resolve("src/" + filePath);
+  const destTextFile = path.resolve("dist/" + filePath);
+  fs.copyFileSync(srcTextFile, destTextFile);
+}
