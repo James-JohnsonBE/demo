d66cec22f77320edbd0e553f9871e102bacc2978
peterfbacklund <backlundpf@state.gov>
2024-08-14 16:55:01 -0400

    update migrations scripts

--- docs/migrate_tasker.js
+++ docs/migrate_tasker.js
@@ -84,3 +84,98 @@ async function shuffleRequestingOffice(request) {
 }
 
 shuffleRequestingOfficesRest();
+
+/******************** MIGRATE RESPONSEDOCSRO ****************************/
+
+async function shuffleResponseDocsRORest() {
+  let uri =
+    `../_api/web/GetListByTitle('AuditResponseDocsRO')/items` +
+    `?$select=ID,RequestNumber,ResponseID,ReqNum,ContentType/Name&$expand=ContentType` +
+    // `&$filter=(ContentType eq 'Document') and (ReqNum eq null)` +
+    `&$top=5000&$skiptoken=`;
+
+  let skiptoken = "Paged=TRUE&p_ID=1";
+  while (uri) {
+    const results = await fetch(uri + skiptoken, {
+      method: "GET",
+      headers: {
+        Accept: "application/json; odata=verbose",
+        "X-RequestDigest": document.getElementById("__REQUESTDIGEST").value,
+      },
+    }).then(async (response) => {
+      if (!response.ok) {
+        uri = null;
+        return [];
+      }
+      const body = await response.json();
+      uri = body.d.__next;
+      return body.d.results.filter(
+        (doc) => doc.ContentType.Name == "Document" && !doc.ReqNum
+      );
+    });
+
+    // request = results.pop();
+    // shuffleResponseDoc(request);
+
+    // uri = false;
+    //await Promise.all(results.map(shuffleResponseDoc));
+    const total = results.length;
+    const window = 300;
+    let start = 0;
+    let end = Math.min(start + window, total);
+    while (end - start > 0) {
+      console.log("Slicing: ", start, end, total);
+      const subset = results.slice(start, end);
+
+      await Promise.all(subset.map(shuffleResponseDoc));
+
+      start = Math.min(start + window, total);
+      end = Math.min(end + window, total);
+
+      await sleep(400);
+      /*
+        count++;
+        console.log(`updating: ${result.ID} - ${count}/${total}`);
+        shuffleResponseDoc(result);
+  
+        if (count % 20 == 0) {
+          await sleep(400);
+        }
+        */
+    }
+  }
+}
+
+async function shuffleResponseDoc(doc) {
+  if (doc.ContentType.Name != "Document" || doc.ReqNum) return false;
+  const uri = `../_api/web/GetListByTitle('AuditResponseDocsRO')/items(${doc.ID})`;
+
+  const result = await fetch(uri, {
+    method: "POST",
+    headers: {
+      Accept: "application/json; odata=verbose",
+      "Content-Type": "application/json; odata=verbose",
+      "X-RequestDigest": document.getElementById("__REQUESTDIGEST").value,
+      "X-HTTP-Method": "MERGE",
+      "If-Match": doc.__metadata.etag,
+    },
+    body: JSON.stringify({
+      __metadata: {
+        type: doc.__metadata.type,
+      },
+      ReqNum: doc.RequestNumber,
+      ResID: doc.ResponseID,
+    }),
+  }).then(async (response) => {
+    if (!response.ok) {
+      console.warn("Could not update request", request, response);
+    }
+  });
+  return true;
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+shuffleResponseDocsRORest();
--- docs/tests.js
+++ docs/tests.js
@@ -3,9 +3,9 @@ var web = currCtx.get_web();
 var responseDocsLib = web.get_lists().getByTitle("AuditResponseDocs");
 var responseDocsQuery = new SP.CamlQuery();
 responseDocsQuery.set_viewXml(
-  '<View Scope="RecursiveAll"><Query>' +
-    '<OrderBy><FieldRef Name="ReqNum"/><FieldRef Name="ResID"/></OrderBy>' +
-    "</Query></View>"
+  '<View><Query><Where><And><BeginsWith><FieldRef Name="Title"/><Value Type="Text">' +
+    "LESAEB-025-2024" +
+    '-</Value></BeginsWith><Eq><FieldRef Name="ContentType" /><Value Type="Text">Folder</Value></Eq></And></Where></Query></View>'
 );
 const m_ResponseDocsItems = responseDocsLib.getItems(responseDocsQuery);
 currCtx.load(
@@ -21,10 +21,9 @@ var web = currCtx.get_web();
 var responseList = web.get_lists().getByTitle("AuditResponses");
 var responseQuery = new SP.CamlQuery();
 responseQuery.set_viewXml(
-  "<View><Query>" +
-    '<Where><Neq><FieldRef Name="ResStatus"/><Value Type="Text">7-Closed</Value></Neq></Where>' +
-    '<OrderBy><FieldRef Name="ReqNum"/></OrderBy>' +
-    "</Query></View>"
+  '<View><Query><Where><Eq><FieldRef Name="ReqNum"/><Value Type="Text">' +
+    oRequest.number +
+    "</Value></Eq></Where></Query></View>"
 );
 const m_responseItems = responseList.getItems(responseQuery);
 //need to check permissions because of granting/removing special perms
@@ -35,73 +34,3 @@ currCtx.load(
 );
 
 currCtx.executeQueryAsync(() => console.log("success"), console.log);
-
-// UPDATED CODE
-console.log("Executing Script");
-let currCtx = new SP.ClientContext.get_current();
-let web = currCtx.get_web();
-var responseDocsLib = web.get_lists().getByTitle("AuditResponseDocs");
-var responseDocsQuery = new SP.CamlQuery();
-responseDocsQuery.set_viewXml(
-  '<View Scope="RecursiveAll"><Query></Query><RowLimit>5000</RowLimit></View>'
-);
-
-var position = new SP.ListItemCollectionPosition();
-position.set_pagingInfo("Paged=TRUE&p_ID=1");
-responseDocsQuery.set_listItemCollectionPosition(position);
-
-responseDocsQuery.get_listItemCollectionPosition();
-
-m_ResponseDocsItems = responseDocsLib.getItems(responseDocsQuery);
-currCtx.load(m_ResponseDocsItems);
-
-currCtx.executeQueryAsync(() => {
-  console.log("Response docs success", m_ResponseDocsItems.get_count());
-
-  console.log(
-    "Items Position",
-    m_ResponseDocsItems.get_listItemCollectionPosition()
-  );
-}, console.log);
-
-async function getAllItems(listTitle) {
-  let listItemsResults = [];
-
-  const currCtx = new SP.ClientContext.get_current();
-  const web = currCtx.get_web();
-
-  const list = web.get_lists().getByTitle(listTitle);
-  const camlQuery = new SP.CamlQuery();
-  camlQuery.set_viewXml(
-    '<View Scope="RecursiveAll"><Query></Query><RowLimit>5000</RowLimit></View>'
-  );
-
-  let position = new SP.ListItemCollectionPosition();
-  position.set_pagingInfo("Paged=TRUE&p_ID=1");
-
-  while (position != null) {
-    camlQuery.set_listItemCollectionPosition(position);
-
-    listItems = list.getItems(camlQuery);
-
-    currCtx.load(listItems);
-
-    await executeQuery(currCtx).catch((sender, args) => {
-      console.warn(sender);
-    });
-
-    const listEnumerator = listItems.getEnumerator();
-    while (listEnumerator.moveNext()) {
-      listItemsResults.push(listEnumerator.get_current());
-    }
-
-    position = listItems.get_listItemCollectionPosition();
-  }
-
-  return listItemsResults;
-}
-
-/* REFERENCES:
-https://sharepoint.stackexchange.com/questions/69650/sp-2010-client-object-model-javascript-how-to-use-get-listitemcollectionpo
-https://sharepoint.stackexchange.com/questions/216562/caml-query-going-around-the-5000-list-item-threshold
-*/
